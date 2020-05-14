95af5386b1dd77ad856c41ee5e3e49b6b3425e08
1736262587 <1736262587@qq.com>
2020-05-14 10:29:50 +0800

    2020.5.14提交

---
+++ 12.1实现图片的浏览功能/PicBr/PicBr/PicBrDoc.h
@@ -0,0 +1,49 @@
+
+// PicBrDoc.h : CPicBrDoc 类的接口
+//
+
+
+#pragma once
+#include "PicBrSet.h"
+
+
+class CPicBrDoc : public CDocument
+{
+protected: // 仅从序列化创建
+	CPicBrDoc();
+	DECLARE_DYNCREATE(CPicBrDoc)
+
+// 特性
+public:
+	CPicBrSet m_PicBrSet;
+
+// 操作
+public:
+
+// 重写
+public:
+	virtual BOOL OnNewDocument();
+#ifdef SHARED_HANDLERS
+	virtual void InitializeSearchContent();
+	virtual void OnDrawThumbnail(CDC& dc, LPRECT lprcBounds);
+#endif // SHARED_HANDLERS
+
+// 实现
+public:
+	virtual ~CPicBrDoc();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	DECLARE_MESSAGE_MAP()
+
+#ifdef SHARED_HANDLERS
+	// 用于为搜索处理程序设置搜索内容的 Helper 函数
+	void SetSearchContent(const CString& value);
+#endif // SHARED_HANDLERS
+};
